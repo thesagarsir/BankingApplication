925ae5ed680d5f4f5452aa46e795259b357f5cdc
ripal bhagat <ripal.bhagat@gmail.com>
2021-01-11 18:38:29 +0530

    RB : Added status method in Controller, Service and Dao
    Signed-off-by: ripal bhagat <ripal.bhagat@gmail.com>

--- SpringBootWithHibernate/src/main/java/sbexample/SpringBootWithHibernate/dao/SpringDao.java
+++ SpringBootWithHibernate/src/main/java/sbexample/SpringBootWithHibernate/dao/SpringDao.java
@@ -68,7 +68,9 @@ public class SpringDao {
 		
 		//sessionfactory.getCurrentSession().load(Employee.class, emp_id);
 		Criteria criteria=session.createCriteria(Employee.class);
+		
 		criteria.add(Restrictions.eq("status", status));
+		
 		List<Employee> list_emp=criteria.list();
 		return list_emp;
 	}
